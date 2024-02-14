5e334689d6ccbfcbfe8f843dd6415a2b3ca8ccbf
JuanHarr <Harringtonjuan@outlook.com>
2024-02-14 13:05:37 -0500

    Creating base project, reaedy to start development

---
+++ TechnicalTestSolvedex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TechnicalTestSolvedex
+//
+//  Created by Juan Harrington on 14/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "e")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, adasworld!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TechnicalTestSolvedex/TechnicalTestSolvedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechnicalTestSolvedexApp.swift
+//  TechnicalTestSolvedex
+//
+//  Created by Juan Harrington on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TechnicalTestSolvedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
