65ec94905a98300eb2e71888198b1ea7dc1d2289
JuanHarr <Harringtonjuan@outlook.com>
2024-02-14 13:20:19 -0500

    Basic setUp to start development

---
+++ TechnicalTestSolvedex/MainImagesView.swift
@@ -0,0 +1,23 @@
+//
+//  MainImagesView.swift
+//  TechnicalTestSolvedex
+//
+//  Created by Juan Harrington on 14/02/24.
+//
+
+import SwiftUI
+
+struct MainImagesView: View {
+    var body: some View {
+        VStack {
+          ScrollView {
+            Text("example")
+          }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+  MainImagesView()
+}
